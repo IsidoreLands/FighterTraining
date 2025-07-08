ffc239428b787b4bf4c2aec946d7bd683dbbbc7b
IsidoreLands <your_email@example.com>
2025-07-08 00:14:42 +0000

    Refine energy_maneuverability.js with Platonic aircraft for MVP testing

--- js/energy_maneuverability.js
+++ js/energy_maneuverability.js
@@ -1,1 +1,51 @@
-// energy_maneuverability.js - EM calculations for FighterTraining\nclass EnergyManeuverability {\n  constructor(thrust, drag, weight, velocity, fuel, maxFuel, militaryBurn, afterburnerBurn, dragCoefficient) {\n    this.thrust = thrust; // Newtons\n    this.drag = drag; // Newtons\n    this.weight = weight; // kg\n    this.velocity = velocity; // m/s\n    this.fuel = fuel; // kg\n    this.maxFuel = maxFuel; // kg\n    this.militaryBurn = militaryBurn; // kg/s\n    this.afterburnerBurn = afterburnerBurn; // kg/s\n    this.dragCoefficient = dragCoefficient; // dimensionless\n  }\n  calculatePs() {\n    return ((this.thrust - this.drag) * this.velocity) / this.weight; // m/s\n  }\n  burnFuel(deltaTime, isAfterburning, isBraking, isTurning) {\n    const burnRate = isAfterburning ? this.afterburnerBurn : this.militaryBurn;\n    this.fuel -= burnRate * deltaTime;\n    this.weight -= burnRate * deltaTime;\n    this.fuel = Math.max(0, this.fuel);\n    this.weight = Math.max(this.maxFuel * 0.5, this.weight); // Prevent unrealistic weight\n    this.drag = this.dragCoefficient * this.velocity * this.velocity;\n    if (isBraking) this.drag += 10000; // Extra drag for braking\n    if (isTurning) this.drag += 15000; // Extra drag for turning\n  }\n  updateVelocity(deltaTime, isAfterburning) {\n    this.thrust = isAfterburning ? this.thrust * 2 : this.thrust; // Simplified afterburner boost\n    const acceleration = (this.thrust - this.drag) / this.weight;\n    this.velocity += acceleration * deltaTime;\n    this.velocity = Math.max(50, Math.min(300, this.velocity)); // Cap velocity\n  }\n}\n// Example Platonic aircraft\n// const platonic = new EnergyManeuverability(60000, 5000, 1000, 160, 60, 60, 0.1, 0.5, 0.85);
+// energy_maneuverability.js - EM calculations for FighterTraining
+class EnergyManeuverability {
+  constructor(thrust, drag, weight, velocity, fuel, maxFuel, militaryBurn, afterburnerBurn, dragCoefficient, turnSpeed) {
+    this.thrust = thrust; // Newtons
+    this.drag = drag; // Newtons
+    this.weight = weight; // kg
+    this.velocity = velocity; // pixels/s (scaled for gameplay)
+    this.fuel = fuel; // kg
+    this.maxFuel = maxFuel; // kg
+    this.militaryBurn = militaryBurn; // kg/s
+    this.afterburnerBurn = afterburnerBurn; // kg/s
+    this.dragCoefficient = dragCoefficient; // dimensionless
+    this.turnSpeed = turnSpeed; // rad/s
+    this.gForce = 1; // g
+    this.isAfterburning = false;
+    this.isBraking = false;
+    this.isTurning = false;
+  }
+  calculatePs() {
+    return ((this.thrust - this.drag) * this.velocity) / this.weight; // pixels/s^2
+  }
+  calculateGForce() {
+    return (this.drag / (9.81 * this.weight)) + 1; // g-force from drag + baseline
+  }
+  burnFuel(deltaTime) {
+    const burnRate = this.isAfterburning ? this.afterburnerBurn : this.militaryBurn;
+    this.fuel -= burnRate * deltaTime;
+    this.weight -= burnRate * deltaTime;
+    this.fuel = Math.max(0, this.fuel);
+    this.weight = Math.max(this.maxFuel * 0.5, this.weight);
+    this.drag = this.dragCoefficient * this.velocity * this.velocity;
+    if (this.isBraking) this.drag += 10000;
+    if (this.isTurning) this.drag += 15000;
+  }
+  updateVelocity(deltaTime) {
+    this.thrust = this.isAfterburning ? this.thrust * 2 : this.thrust;
+    const acceleration = (this.thrust - this.drag) / this.weight;
+    this.velocity += acceleration * deltaTime;
+    this.velocity = Math.max(50, Math.min(300, this.velocity));
+  }
+  update(deltaTime, inputs) {
+    this.isAfterburning = inputs.isAfterburning || false;
+    this.isBraking = inputs.isBraking || false;
+    this.isTurning = inputs.isTurning || false;
+    this.burnFuel(deltaTime);
+    this.updateVelocity(deltaTime);
+    this.gForce = this.calculateGForce();
+  }
+}
+// Platonic aircraft for testing
+// const platonic = new EnergyManeuverability(60000, 5000, 1000, 160, 60, 60, 0.1, 0.5, 0.85, 0.06);
